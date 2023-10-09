0a3f80269e77a55446a6e952f1635f6e765ab0f3
glennga <ggalvizo@uci.edu>
2023-10-08 22:46:09 -0700

    Updating the Graphix files (and including a subjects folder).

--- subjects/graphix/bin/setup.sh
+++ subjects/graphix/bin/setup.sh
@@ -11,9 +11,10 @@ DIST_PATH=`dirname "${OPT_PATH}"`
 CC_PUBLIC_ADDRESS=$(grep cc ${AWS_PATH}/conf/instance/inventory -A 1 | tail -n 1)
 CC_PRIVATE_ADDRESS=$(grep cc ${AWS_PATH}/conf/instance/cc.conf -A 1 | tail -n 1 | cut -d "=" -f 2)
 
-for i in 1 2 3 4 5 6; do
+for i in 1 2 3a 3b 3c 3d 3e 3f 3g 3h 3i 4 5; do
   python3 ~/setup/run-statement.py \
     --file ~/setup/create-s$i.sqlpp \
     --uri http://$CC_PUBLIC_ADDRESS:19002 \
     --sub DATA_PATH,$CC_PRIVATE_ADDRESS:///data1/raw-data/
+  sleep 5
 done
